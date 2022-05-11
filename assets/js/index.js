17fba4293e38c3c1ae640471f6e2f7b97536b6e2
binhho1099 <binhho1099@gmail.com>
2022-05-11 18:37:45 +0700

    bug s2 slider and bug popup product

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,142 +1,28 @@
-const btnWave2S3 = document.querySelector('.btn-wave2-s3');
-const btnWave1S3 = document.querySelector('.btn-wave1-s3');
-const btnWave2S32 = document.querySelector('.btn-wave2-s3-2');
-const btnWave1S32 = document.querySelector('.btn-wave1-s3-2');
-const character = document.querySelector('.s3-bottom__product');
-const character2 = document.querySelector('.s3-bottom__product-2');
-const listProduct1 = document.querySelector('.js-list-product-1');
-const listProduct2 = document.querySelector('.js-list-product-2');
-const modalContent = document.querySelector('#modal-content');
+// btn wave
+const wave1Active = document.querySelector('.wave-1-active');
+const wave1MiniActive = document.querySelector('.wave-1-mini-active');
+const wave2Active = document.querySelector('.wave-2-active');
+const wave2MiniActive = document.querySelector('.wave-2-mini-active');
 
-const app = {
-  data: {
-    racing: [
-      'Racing_1',
-      'Racing_2',
-      'Racing_3',
-      'Racing_4',
-      'Racing_5',
-      'Racing_6',
-      'Racing_7',
-      'Racing_8',
-      'Racing_9',
-      'Racing_10',
-      'Racing_11',
-      'Racing_12',
-      'Racing_13',
-      'Racing_14',
-      'Racing_15',
-    ],
-    mini: ['Mini_1', 'Mini_2', 'Mini_3', 'Mini_4', 'Mini_5', 'Mini_6'],
-    racing_w2: [
-      'Racing_W2_1',
-      'Racing_W2_2',
-      'Racing_W2_3',
-      'Racing_W2_4',
-      'Racing_W2_5',
-      'Racing_W2_6',
-      'Racing_W2_7',
-      'Racing_W2_8',
-      'Racing_W2_9',
-      'Racing_W2_10',
-      'Racing_W2_11',
-      'Racing_W2_12',
-      'Racing_W2_13',
-      'Racing_W2_14',
-      'Racing_W2_15',
-    ],
-    mini_w2: [
-      'Mini_W2_1',
-      'Mini_W2_2',
-      'Mini_W2_3',
-      'Mini_W2_4',
-      'Mini_W2_5',
-      'Mini_W2_6',
-    ],
-  },
-  img: `<div class="col-12 text-center">
-  <img
-    class="s3-bottom__product-headline"
-    src="./assets/img/Assetss/tap-to-learn-red.png"
-    alt=""
-  />
-</div>`,
-  dataListProduct1: function (data) {
-    return `<div class="s3-bottom__card col-6 col-sm-4 col-lg-12d5">
-    <img
-      onclick="app.showModal('Popup_${data}')"
-      src="./assets/img/Assetss/Character Cards/${data}.png"
-      alt=""
-    />
-  </div>`;
-  },
-  dataListProduct2: function (data) {
-    return `<div class="col-6 col-md-4 s3-bottom__card-2">
-    <img
-      onclick="app.showModal('Popup_${data}')"
-      src="./assets/img/Assetss/Character Cards/${data}.png"
-      alt=""
-    />
-  </div>`;
-  },
-  handleEvent: function () {
-    btnWave1S3.onclick = function () {
-      btnWave1S3.classList.remove('disable');
-      btnWave2S3.classList.add('disable');
-      character.style.borderColor = '#44FFFF';
-      const htmls = app.data.racing
-        .map(item => {
-          return app.dataListProduct1(item);
-        })
-        .join('');
-      const string = app.img.concat(htmls);
-      listProduct1.innerHTML = string;
-    };
-    btnWave2S3.onclick = function () {
-      btnWave2S3.classList.remove('disable');
-      btnWave1S3.classList.add('disable');
-      character.style.borderColor = '#fc486b';
-      const htmls = app.data.racing_w2
-        .map(item => {
-          return app.dataListProduct1(item);
-        })
-        .join('');
-      const string = app.img.concat(htmls);
-      listProduct1.innerHTML = string;
-    };
+// list product
+const racingVenomized = document.querySelector('.s3__racing-venomized');
+const racingOriginal = document.querySelector('.s3__racing-original');
+const miniVenomized = document.querySelector('.s3__mini-venomized');
+const miniOriginal = document.querySelector('.s3__mini-original');
 
-    btnWave2S32.onclick = function () {
-      btnWave2S32.classList.remove('disable');
-      btnWave1S32.classList.add('disable');
-      character2.style.borderColor = '#fc486b';
-      const htmls = app.data.mini_w2
-        .map(item => {
-          return app.dataListProduct2(item);
-        })
-        .join('');
-      listProduct2.innerHTML = htmls;
-    };
-    btnWave1S32.onclick = function () {
-      btnWave1S32.classList.remove('disable');
-      btnWave2S32.classList.add('disable');
-      character2.style.borderColor = '#44FFFF';
-      const htmls = app.data.mini
-        .map(item => {
-          return app.dataListProduct2(item);
-        })
-        .join('');
-      listProduct2.innerHTML = htmls;
-    };
-  },
-  showModal: function (data) {
-    modalContent.innerHTML = `<img
-    src="./assets/img/Assetss/Popup/${data}.png"
-    alt=""
-  />`;
-    $('#myModal').modal('show');
-  },
-  run: function () {
-    this.handleEvent();
-  },
-};
-app.run();
+// ********************* handle action ******************************
+function handleChanceListProductRacing() {
+  racingOriginal.classList.toggle('hiden');
+  racingVenomized.classList.toggle('hiden');
+}
+
+wave1Active.addEventListener('click', handleChanceListProductRacing);
+wave2Active.addEventListener('click', handleChanceListProductRacing);
+
+function handleChanceListProductMini() {
+  miniOriginal.classList.toggle('hiden');
+  miniVenomized.classList.toggle('hiden');
+}
+
+wave1MiniActive.addEventListener('click', handleChanceListProductMini);
+wave2MiniActive.addEventListener('click', handleChanceListProductMini);
--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,54 +1,87 @@
 const sliderS2ListItem = document.querySelectorAll('.s2__item');
 const video = document.querySelector('.s2__video-item');
 const thumbnailVideo = document.querySelector('.s2__video-thumbnail');
-let thumbnailActive = 1;
 // JS VIDEO SLIDER
 const handleActiveThumbnail = id => {
   sliderS2ListItem.forEach(item => {
     item.classList.remove('active');
-    console.log('binh');
   });
   sliderS2ListItem.forEach(i => {
     if (id == i.dataset.id) {
       i.classList.add('active');
-      thumbnailActive = i.dataset.id;
     }
   });
 };
-handleActiveThumbnail(thumbnailActive);
+
+// handleActiveThumbnail(1);
 function chanceVideo(id) {
   video.setAttribute('src', `./assets/videos/video_${id}.mp4`);
   video.play();
 }
+
 function handleClickSelectItemSlider() {
   const id = this.dataset.id;
-  handleActiveThumbnail(id);
-  chanceVideo(id);
-  console.log(id);
 }
-
-sliderS2ListItem.forEach(i =>
-  i.addEventListener('click', handleClickSelectItemSlider)
-);
-
-// carousel
-$('.owl-carousel').owlCarousel({
-  // loop: true,
-  margin: 20,
-  dots: false,
-  nav: true,
-
-  responsive: {
-    0: {
-      items: 2.5,
-      nav: false,
+$(document).ready(function () {
+  // carousel modal
+  $('.owl-modal').owlCarousel({
+    loop: true,
+    items: 1,
+    margin: 10,
+    dots: false,
+    nav: true,
+    navText: [
+      '<img src="./assets/img/Assetss/arrow_left.png" alt="arrow_left">',
+      '<img src="./assets/img/Assetss/arrow_right.png" alt="arrow_right">',
+    ],
+  });
+  // corousel video s2
+  $('.owl-video').owlCarousel({
+    loop: true,
+    // rewind: true,
+    margin: 20,
+    dots: false,
+    nav: true,
+    responsive: {
+      0: {
+        items: 2.5,
+        nav: false,
+      },
+      768: {
+        items: 3,
+        navText: [
+          '<img src="./assets/img/Assetss/arrow_left.png" alt="arrow_left">',
+          '<img src="./assets/img/Assetss/arrow_right.png" alt="arrow_right">',
+        ],
+      },
     },
-    768: {
-      items: 3,
-      navText: [
-        '<img src="./assets/img/Assetss/arrow_left.png" alt="arrow_left">',
-        '<img src="./assets/img/Assetss/arrow_right.png" alt="arrow_right">',
-      ],
-    },
-  },
+  });
+  handleActiveThumbnail(1);
+  $('.owl-video').on('click', '.item', function (e) {
+    const id = e.target.parentElement.dataset.id;
+    handleActiveThumbnail(id);
+    // handleActiveThumbnail();
+    // e.target.parentElement.classList.add('active');
+    chanceVideo(id);
+  });
+
+  const products = document.querySelectorAll('.s3-bottom__product');
+  products.forEach(product =>
+    product.addEventListener('click', function (e) {
+      const itemModalName = e.target.dataset.modalName;
+      const index = e.target.dataset.indexModal;
+      const modals = document.querySelectorAll('.owl-modal');
+      $(`#owl-modal-${itemModalName}`).trigger('to.owl.carousel', [
+        index,
+        1,
+        true,
+      ]);
+      modals.forEach(modal =>
+        modal.dataset.modalName === itemModalName
+          ? modal.classList.remove('hiden')
+          : modal.classList.add('hiden')
+      );
+      $('#myModal').modal('show');
+    })
+  );
 });
